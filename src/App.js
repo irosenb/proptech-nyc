2494e0e7c9b42ec446c8ec1c9bcf45a27df0fd8c
Isaac Rosenberg <irosenb7@gmail.com>
2023-08-04 19:53:11 -0400

    Hello world proptech!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, DatePicker } from 'antd';
+import { useEffect } from 'react';
+import Airtable from 'airtable';
+
+const base = new Airtable({base_key: process.env.REACT_APP_AIRTABLE_KEY}).base('appkgFanuGmwzHgbv')
 
 function App() {
+  useEffect(async () => {
+    const records = await base.select().all()
+    console.log(records);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>NYC Proptech</h1>
+      <Button size='large'>Press me</Button>
     </div>
   );
 }
