d0b53c5f21a7d56de41623ef6ca174d69fbd9d9a
Isaac Rosenberg <irosenb7@gmail.com>
2023-08-05 09:48:46 -0400

    Abstracted Event into its own class to manage UI

--- src/components/proptech.js
+++ src/components/proptech.js
@@ -5,26 +5,37 @@ import Airtable from 'airtable';
 const base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_KEY}).base('appkgFanuGmwzHgbv')
 
 const Proptech = () => {
-    const [records, setRecords] = useState([]) 
+  const [records, setRecords] = useState([]) 
 
-    useEffect(() => {  
-        callRecords()
-    }, [])
+  useEffect(() => {  
+    callRecords()
+  }, [])
 
-    const callRecords = async () => {
-        const records = await base('tblipl9Nu1Q2ylZLs').select().all()
-        console.log(records);
+  const callRecords = async () => {
+    const records = await base('tblipl9Nu1Q2ylZLs').select().all()
+    console.log(records);
 
-        setRecords(records)
-    } 
+    setRecords(records)
+  } 
 
-    return (
-        <div className="App">
-        <h1>NYC Proptech</h1>
-        <Button size='large'>Press me</Button>
-        <p>{records.map(record => record.get('Name'))}</p>
-        </div>
-    )
+  return (
+    <div className="App">
+      <h1>NYC Proptech</h1>
+      <Button size='large'>Press me</Button>
+      {records.map(record => (
+        <Event key={record.id} record={record}/>
+      ))}
+      <p></p>
+    </div>
+  )
+}
+
+function Event({record}) {
+  return (
+    <div className='record'>
+      <h1>{record.fields.Name}</h1>
+    </div>
+  )
 }
 
 export default Proptech
